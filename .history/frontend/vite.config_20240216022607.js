8a23b483ae314e9e67244a387311cbacfdefd61c
nlsukhde <nsukhdeo10@gmail.com>
2024-02-16 02:55:58 -0500

    Stop tracking .history folder

--- .history/frontend/vite.config_20240216022607.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/CSE442-542/2024-Spring/cse-442l/',
-  plugins: [react()]
-})
