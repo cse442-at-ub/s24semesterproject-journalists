57bb180906a32ce24b00b9c64938534f94c5412c
nlsukhde <102875950+nlsukhde@users.noreply.github.com>
2024-02-25 04:16:35 -0500

    created create.php for entries

---
+++ backend/journal/create.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../config/config.php';
+
+// Enabling CORS for local development
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
