91f5202d64a529f35acc25d02fcdb250ce8eb010
Ahmed <ahmedbouhraoua0105@gmail.com>
2024-03-01 00:35:41 -0500

    #33 added responsive signup and login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,20 @@ import EditProfile from './EditProfile.jsx';
 import Journal from "./Journal_Dashboard";
 import JournalImage from "./Jornal_image";
 import JournalVideo from "./Journal_video";
+import SignupPage from './SignupPage.jsx';
+import LoginPage from './LoginPage.jsx';
+import Journal_Dashboard from './Journal_Dashboard';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Journal />} />
+        <Route path="/" element={<SignupPage />} />
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/journal-image" element={<JournalImage />} />
         <Route path="/journal-video" element={<JournalVideo />} />
+        <Route path="/signup-page" element={<SignupPage />} />
+        <Route path="/login-page" element={<LoginPage />} />
       </Routes>
     </Router>
   );
