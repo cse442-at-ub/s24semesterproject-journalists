a5589e6eec3bf14c2b0a751b134de6a6a53e20e8
beibitzh <beibitzh@buffalo.edu>
2024-03-06 20:11:55 -0500

    making improvments for mobile responcive

--- frontend/src/Jornal_image.jsx
+++ frontend/src/Jornal_image.jsx
@@ -51,7 +51,7 @@ const JournalImage = () => {
             </button>
           ))}
         </div>
-        <button className="button orange logout-button" onClick={handleLogout}>Logout</button>
+        <button className="logout-btn" onClick={handleLogout}>Logout</button>
       </div>
       <div className="right-column">
         <p className="date">Date: {new Date().toLocaleDateString()}</p>
